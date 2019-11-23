342541495869b5c30184e7731595d3bc02e2142f
Aleks <osinaleksandr1989@gmail.com>
2019-11-23 17:58:59 +0500

    выполнил разделение на модули поправил eslintrc - sourcetype module

---
+++ src/components/trip-info.js
@@ -0,0 +1,9 @@
+export const createTripInfoTemplate = () => {
+  return (
+    `<div class="trip-info__main">
+      <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
+
+      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
+    </div>`
+  );
+};
