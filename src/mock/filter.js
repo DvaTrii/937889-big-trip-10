65a6e46f92f3c261ea370eba268bb4573303cbe1
Aleks <osinaleksandr1989@gmail.com>
2019-12-24 11:23:21 +0500

    3.3 Создает элементы меню. Исправляет Удаляет разметку из  моков фильтра

---
+++ src/mock/filter.js
@@ -0,0 +1,14 @@
+const filters = [
+  {
+    name: `Everything`,
+    isChecked: true
+  },
+  {
+    name: `Future`,
+    isChecked: false
+  },
+  {
+    name: `Past`,
+    isChecked: false
+  },
+];
--- src/mock/menu.js
+++ src/mock/menu.js
@@ -1,9 +1,1 @@
-export const createSiteMenuTemplate = () => {
-  return (
-    `<h2 class="visually-hidden">Switch trip view</h2>
-     <nav class="trip-controls__trip-tabs  trip-tabs">
-        <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-        <a class="trip-tabs__btn" href="#">Stats</a>
-     </nav>`
-  );
-};
+export const menu = [`Table`, `Stats`];
