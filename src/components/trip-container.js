ad35a605e868d9be2d2142fd1901435c2519c693
Aleks <osinaleksandr1989@gmail.com>
2020-01-02 12:59:41 +0500

    4.1.7 Перепишет на классы tripContainer

--- src/components/trip-container.js
+++ src/components/trip-container.js
@@ -1,3 +1,27 @@
+import {createElement} from "../utils";
+
 export const createTripContainerTemplate = () => {
   return (`<ul class="trip-days"></ul>`);
 };
+
+export default class TripContainer {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripContainerTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
