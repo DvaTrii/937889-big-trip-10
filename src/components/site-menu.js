fe9054da02fb99a2f7d503ce3f5c2f58b07eee78
Aleks <osinaleksandr1989@gmail.com>
2020-01-02 12:18:10 +0500

    4.1.2 Перепишет на классы меню

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -1,4 +1,6 @@
-export const createSiteMenuTemplate = () => {
+import {createElement} from "../utils";
+
+const createSiteMenuTemplate = () => {
   return (
     `<h2 class="visually-hidden">Switch trip view</h2>
      <nav class="trip-controls__trip-tabs  trip-tabs">
@@ -7,3 +9,26 @@ export const createSiteMenuTemplate = () => {
      </nav>`
   );
 };
+
+export default class SiteMenu {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSiteMenuTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
