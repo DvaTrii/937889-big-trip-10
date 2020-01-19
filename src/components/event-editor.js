deb3ed886e528d5c2f047e08f9962de7f82c82e0
Aleks <osinaleksandr1989@gmail.com>
2020-01-19 11:06:41 +0500

    Вносит исправления в разметку event-editor

--- src/components/event-editor.js
+++ src/components/event-editor.js
@@ -137,7 +137,18 @@ const createEditEventTemplate = (dayEvent) => {
             </div>
 
             <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-            <button class="event__reset-btn" type="reset">Cancel</button>
+            <button class="event__reset-btn" type="reset">Delete</button>
+            <input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" checked>
+            <label class="event__favorite-btn" for="event-favorite-1">
+              <span class="visually-hidden">Add to favorite</span>
+              <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+                <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+              </svg>
+            </label>
+
+            <button class="event__rollup-btn" type="button">
+              <span class="visually-hidden">Open event</span>
+            </button>
           </header>
           <section class="event__details">
 
