8dd14efc8175db039622acfa784b99e5c6fca3b9
Aleks <osinaleksandr1989@gmail.com>
2020-01-21 16:08:19 +0500

    Добавит виды сортиовки

--- src/components/sorter.js
+++ src/components/sorter.js
@@ -1,25 +1,31 @@
 import AbstractComponent from "./abstract-component.js";
 
+export const SortType = {
+  TIME: `time`,
+  PRICE: `price`,
+  EVENT: `event`,
+};
+
 const createSortTemplate = () => {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <span class="trip-sort__item  trip-sort__item--day"></span>
 
       <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event">
-        <label class="trip-sort__btn" for="sort-event">Event</label>
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
+        <label class="trip-sort__btn" for="sort-event" data-sort-type="${SortType.EVENT}">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" checked>
-        <label class="trip-sort__btn  trip-sort__btn--active  trip-sort__btn--by-increase" for="sort-time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <label class="trip-sort__btn" for="sort-time" data-sort-type="${SortType.TIME}">
           Time
         </label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
         <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">
+        <label class="trip-sort__btn" for="sort-price" data-sort-type="${SortType.PRICE}">
           Price
         </label>
       </div>
@@ -30,6 +36,11 @@ const createSortTemplate = () => {
 };
 
 export default class Sorter extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currenSortType = SortType.EVENT;
+  }
 
   getTemplate() {
     return createSortTemplate();
