d726434aae46398d3e976245a119ab5f2f0b1670
Aleks <osinaleksandr1989@gmail.com>
2020-01-21 15:54:07 +0500

    ===== Заменяет генерацию фильтра на статическую разметку =====

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import SiteMenuComponent from "./components/site-menu.js";
 import FilterComponent from "./components/filters.js";
 import TripController from "./controller/trip.js";
 
-import {filters} from "./mock/filter";
 import {tripEvents, dates} from "./mock/mock.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const siteMenuContainer = document.querySelector(`.trip-controls`);
 
 render(siteMenuContainer, new SiteMenuComponent(), RenderPosition.BEFOREEND);
-render(siteMenuContainer, new FilterComponent(filters), RenderPosition.BEFOREEND);
+render(siteMenuContainer, new FilterComponent(), RenderPosition.BEFOREEND);
 
 const siteContentContainer = document.querySelector(`.trip-events`);
 
