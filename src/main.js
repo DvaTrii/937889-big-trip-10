d5f88b21b1585838bd4c5e209ac6404d364cc8be
Aleks <osinaleksandr1989@gmail.com>
2020-01-30 12:09:39 +0500

    7.1.4 Подключит модель в точку входа

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import SiteMenuComponent from "./components/site-menu.js";
 import FilterComponent from "./components/filters.js";
 import TripController from "./controllers/trip-controller.js";
+import PointsModel from "./models/points-model.js";
 
 import {tripEvents} from "./mock/mock.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -11,6 +12,9 @@ render(siteMenuContainer, new SiteMenuComponent(), RenderPosition.BEFOREEND);
 render(siteMenuContainer, new FilterComponent(), RenderPosition.BEFOREEND);
 
 const siteContentContainer = document.querySelector(`.trip-events`);
+
+const pointsModel = new PointsModel();
+pointsModel.setPoints(tripEvents);
 
 const tripController = new TripController(siteContentContainer);
 
