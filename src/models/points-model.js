5319df1ae1f210dc9e548a036fd50e17e04554ca
Aleks <osinaleksandr1989@gmail.com>
2020-01-30 11:47:11 +0500

    7.1.2 Добавит методы для получения и записи точек маршрута

--- src/models/points-model.js
+++ src/models/points-model.js
@@ -2,4 +2,12 @@ export default class PointsModel {
   constructor() {
     this._points = [];
   }
+
+  getPoints() {
+    return this._points;
+  }
+
+  setPoints(points) {
+    this._points = Array.from(points);
+  }
 }
