528d514e446f045b4eaa73d6471f69c0cd41c84c
Aleks <osinaleksandr1989@gmail.com>
2020-01-31 21:17:12 +0500

    7.1.11 подключит фильтры в точку входа

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import SiteMenuComponent from "./components/site-menu.js";
-import FilterComponent from "./components/filters.js";
+// import FilterComponent from "./components/filters.js";
 import TripController from "./controllers/trip-controller.js";
+import FilterController from "./controllers/filter-controller.js";
 import PointsModel from "./models/points-model.js";
 
 import {tripEvents} from "./mock/mock.js";
@@ -9,13 +10,15 @@ import {render, RenderPosition} from "./utils/render.js";
 const siteMenuContainer = document.querySelector(`.trip-controls`);
 
 render(siteMenuContainer, new SiteMenuComponent(), RenderPosition.BEFOREEND);
-render(siteMenuContainer, new FilterComponent(), RenderPosition.BEFOREEND);
+// render(siteMenuContainer, new FilterComponent(), RenderPosition.BEFOREEND);
 
 const siteContentContainer = document.querySelector(`.trip-events`);
 
 const pointsModel = new PointsModel();
 pointsModel.setPoints(tripEvents);
 
+const filterController = new FilterController(siteMenuContainer, pointsModel);
+filterController.render();
 const tripController = new TripController(siteContentContainer, pointsModel);
 
 tripController.render();
