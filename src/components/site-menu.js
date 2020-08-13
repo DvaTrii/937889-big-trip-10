077291c5175a8a22f1611b9bde34e5d99c61682b
Aleks <osinaleksandr1989@gmail.com>
2020-08-13 20:09:57 +0500

    7.1.33 Добавит в компонент меню интерактивность - методы выбора элемента меню

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -1,27 +1,59 @@
 import AbstractComponent from "./abstract-component.js";
 import {MenuItems} from "../const.js";
 
-const createMenuItemsMarkup = (menuItems) => {
+const ACTIVE_MENU_ITEM_CLASS = `trip-tabs__btn--active`;
+
+const createSiteMenuItemsMarkup = (menuItems, currentSiteMenuItem) => {
   return menuItems.map((menuItem) => {
     return (
-      `<a class="trip-tabs__btn" href="#">${menuItem}</a>`
+      `<a class="trip-tabs__btn ${menuItem === currentSiteMenuItem ? ACTIVE_MENU_ITEM_CLASS : ``}" href="#">${menuItem}</a>`
     );
   }).join(`\n`);
 };
 
-const createSiteMenuTemplate = (menuItems) => {
+const createSiteMenuTemplate = (menuItems, currentSiteMenuItem) => {
   return (
     `<div>
        <h2 class="visually-hidden">Switch trip view</h2>
        <nav class="trip-controls__trip-tabs  trip-tabs">
-          ${createMenuItemsMarkup(menuItems)}
+          ${createSiteMenuItemsMarkup(menuItems, currentSiteMenuItem)}
        </nav>
      </div>`
   );
 };
 
 export default class SiteMenu extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSiteMenuItem = MenuItems.TABLE;
+  }
+
   getTemplate() {
-    return createSiteMenuTemplate(Array.from(Object.values(MenuItems)));
+    return createSiteMenuTemplate(Array.from(Object.values(MenuItems)), this._currentSiteMenuItem);
+  }
+
+  setSiteMenuItemClickHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      this._currentMenuItem = MenuItems[evt.target.textContent.toUpperCase()];
+
+      handler(this._currentMenuItem);
+
+      this._setItemActiveClass(evt.target);
+    });
+  }
+
+  _setItemActiveClass(item) {
+    const menuItems = Array.from(this.getElement().querySelectorAll(`.trip-tabs__btn`));
+
+    menuItems.forEach((menuItem) => {
+      menuItem.classList.remove(ACTIVE_MENU_ITEM_CLASS);
+    });
+
+    item.classList.add(ACTIVE_MENU_ITEM_CLASS);
   }
 }
