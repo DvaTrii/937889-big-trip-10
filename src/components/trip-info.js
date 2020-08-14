e6cf2f45ba7b4ac28ce3d14b21990a1a1e219b2e
Aleks <osinaleksandr1989@gmail.com>
2020-08-14 15:40:10 +0500

    7.1.35 Перенесет логику расчета общей стоимости на класс tripInfo

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -2,13 +2,7 @@ import AbstractComponent from "./abstract-component.js";
 import {getFormatDate} from "../utils/common.js";
 import {customMonth} from "../utils/common.js";
 
-const createTripInfoTemplate = (data) => {
-
-  const pointsPrice = (points) => points.reduce((acc, {price}) => acc + Number(price), 0);
-  const allOffersPrice = (points) => points.map((it) => it.offers.filter((iit) => iit.isChecked)
-                                                                     .reduce((accum, {offerPrice}) => accum + Number(offerPrice), 0))
-                                           .reduce((acc, price) => acc + price, 0);
-
+const createTripInfoTemplate = (data, price) => {
   return (
     `<section class="trip-main__trip-info  trip-info">
         <div class="trip-info__main">
@@ -20,7 +14,7 @@ const createTripInfoTemplate = (data) => {
                                          ${getFormatDate(data[data.length - 1].endDate)}</p>
         </div>
         <p class="trip-info__cost">
-                  Total: &euro;&nbsp;<span class="trip-info__cost-value">${pointsPrice(data) + allOffersPrice(data)}</span>
+                  Total: &euro;&nbsp;<span class="trip-info__cost-value">${price}</span>
         </p>
     </section>`
   );
@@ -30,9 +24,19 @@ export default class TripInfo extends AbstractComponent {
   constructor(data) {
     super();
     this._data = data;
+    this._price = this._countPrice(this._data);
   }
 
   getTemplate() {
-    return createTripInfoTemplate(this._data);
+    return createTripInfoTemplate(this._data, this._price);
+  }
+
+  _countPrice() {
+    const pointsPrice = (points) => points.reduce((acc, {price}) => acc + Number(price), 0);
+    const allOffersPrice = (points) => points.map((point) => point.offers.filter((offer) => offer.isChecked)
+      .reduce((accum, {offerPrice}) => accum + Number(offerPrice), 0))
+      .reduce((acc, price) => acc + price, 0);
+
+    return pointsPrice(this._data) + allOffersPrice(this._data);
   }
 }
