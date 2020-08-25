db17f3ddcac5e3cb64a111f1e23ace29a513517a
Aleks <osinaleksandr1989@gmail.com>
2020-08-25 16:26:10 +0500

    8.1.6 Создаст дополнительные методы для работы с данными в point-model

--- src/models/point-model.js
+++ src/models/point-model.js
@@ -5,11 +5,28 @@ export default class Point {
     this.place = data[`destination`][`name`];
     this.startDate = new Date(data[`date_from`]).getTime();
     this.endDate = new Date(data[`date_to`]).getTime();
-    this.price = data[`base_price`];
+    this.price = Number(data[`base_price`]);
     this.offers = data[`offers`];
     this.description = data[`destination`][`description`];
-    this.isFavorite = data[`is_favorite`];
+    this.isFavorite = Boolean(data[`is_favorite`]);
     this.photos = data[`destination`][`pictures`];
+  }
+
+  toRAW() {
+    return {
+      base_price: Number(this.price),
+      date_from: new Date(this.startDate).toISOString(),
+      date_to: new Date(this.endDate).toISOString(),
+      destination: {
+        description: this.description,
+        name: this.place,
+        pictures: this.photos
+      },
+      id: this.id,
+      is_favorite: this.isFavorite,
+      offers: this.offers,
+      type: this.type,
+    };
   }
 
   static parsePoint(data) {
@@ -18,6 +35,10 @@ export default class Point {
 
   static parsePoints(data) {
     return data.map(Point.parsePoint);
+  }
+
+  static clone(data) {
+    return new Point(data.toRAW());
   }
 
 }
