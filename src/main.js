b8fc3dd970ba7c553fafdda3c00ea72e3e54ed2a
Aleks <osinaleksandr1989@gmail.com>
2020-09-12 18:24:05 +0500

    == Создаст app-controller, подключит и проинициализирует его в main.js ==

--- src/main.js
+++ src/main.js
@@ -1,55 +1,5 @@
-import API from "./api.js";
-import SiteMenuComponent from "./components/site-menu.js";
-import StatisticsComponent from "./components/statistics.js";
-import TripController from "./controllers/trip-controller.js";
-import FilterController from "./controllers/filter-controller.js";
-import PointsModel from "./models/points-model.js";
-import {MenuItems} from "./const.js";
+import AppController from "./controllers/app-controller";
 
-import {render, RenderPosition} from "./utils/render.js";
+const app = new AppController();
 
-const AUTHORIZATION = `Basic dXNchar9yZAo=`;
-const END_POINT = `https://htmlacademy-es-10.appspot.com/big-trip`;
-
-const api = new API(END_POINT, AUTHORIZATION);
-
-const pointsModel = new PointsModel();
-
-const siteMenuContainer = document.querySelector(`.trip-controls`);
-const siteMenu = new SiteMenuComponent();
-render(siteMenuContainer, siteMenu, RenderPosition.BEFOREEND);
-
-const filterController = new FilterController(siteMenuContainer, pointsModel);
-filterController.render();
-
-const siteContentContainer = document.querySelector(`.trip-events`);
-const tripController = new TripController(siteContentContainer, pointsModel, api);
-
-const bodyContainer = document.querySelector(`.page-main .page-body__container`);
-const statisticsComponent = new StatisticsComponent();
-render(bodyContainer, statisticsComponent, RenderPosition.BEFOREEND);
-statisticsComponent.hide();
-
-document.querySelector(`.trip-main__event-add-btn`)
-  .addEventListener(`click`, () => {
-    tripController.createPoint();
-  });
-
-siteMenu.setSiteMenuItemClickHandler((currentSiteMenuItem) => {
-  switch (currentSiteMenuItem) {
-    case MenuItems.TABLE:
-      tripController.show();
-      statisticsComponent.hide();
-      break;
-    case MenuItems.STATS:
-      statisticsComponent.show();
-      tripController.hide();
-      break;
-  }
-});
-
-Promise.all(api.getAllData())
-  .then((values) => {
-    pointsModel.setPoints(values[0]);
-    tripController.render();
-  });
+app.init();
